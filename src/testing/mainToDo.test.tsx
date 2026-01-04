59e644d17f02d7fd1814d3c1bdbb93c566193c7d
ruku <hamaro1818@gmail.com>
2026-01-04 14:10:40 +0900

    feat: ToDoの追加.

---
+++ src/testing/mainToDo.test.tsx
@@ -0,0 +1,8 @@
+import "@testing-library/jest-dom";
+
+describe("シーンの分割をする", () => {
+    it.todo("「ホーム画面」・「日記一覧」・「カレンダー」のボタンを設置");
+    it.todo("「ホーム画面」:/")
+    it.todo("「日記一覧」:/diaries?...");
+    it.todo("「カレンダー」:/calendar");
+})
