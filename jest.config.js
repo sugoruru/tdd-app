4d733f8afa42373e7bebe3baa447c64eae12c8aa
loop0919 <ruupu2001@gmail.com>
2026-01-04 13:45:42 +0900

    loop: Test が動くようにする

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,12 @@
-const { createDefaultPreset } = require("ts-jest");
+import { createDefaultPreset } from "ts-jest";
 
 const tsJestTransformCfg = createDefaultPreset().transform;
 
 /** @type {import("jest").Config} **/
 export default {
-  testEnvironment: "node",
-  transform: {
-    ...tsJestTransformCfg,
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
   },
 };
---
+++ src/testing/test.test.tsx
@@ -0,0 +1,3 @@
+import "@testing-library/jest-dom";
+
+it.todo("Hello TDD");
